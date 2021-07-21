9d8f7f2b3a94d46b5c7ba18a1d70cc4665071814
TheNexusAvenger <13441476+TheNexusAvenger@users.noreply.github.com>
2021-07-21 18:21:08 -0400

    Add command for toggling.

--- src/init.client.lua
+++ src/init.client.lua
@@ -4,13 +4,58 @@ TheNexusAvenger
 Loads Nexus VR Compatibility Tester.
 --]]
 
+local OPEN_PREFIX = ":"
+local OPEN_COMMAND = "vrcheck"
+
+
+
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
 local TestListView = require(script:WaitForChild("UI"):WaitForChild("TestListView"))
 
-
+local CmdrSetUp = false
 
 --Create the list view.
 local Tests = TestListView.new()
 if script.Parent.Name == "PlayerGui" then
     script.Parent = Tests.ScreenGui
 end
-Tests.Visible = true
+
+
+
+--[[
+Sets up the open command with Cmdr.
+--]]
+local function SetUpCmdr()
+    if CmdrSetUp then return end
+
+    --Return if Cmdr is not detected.
+    local CmdrClient = ReplicatedStorage:FindFirstChild("CmdrClient")
+    if not CmdrClient then return end
+
+    --Load the command.
+    local Cmdr = require(CmdrClient)
+    Cmdr.Registry:RegisterCommandObject({
+        Name = OPEN_COMMAND,
+        Description = "Opens Nexus VR Compatibility Tester.",
+        Group = "Debug",
+        Args = {},
+        ClientRun = function()
+            Tests.Visible = not Tests.Visible
+            return "Nexus VR Compatibility Tester "..(Tests.Visible and "opened" or "closed").."."
+        end,
+    })
+end
+
+
+
+--Connect a player chatting the open command.
+Players.LocalPlayer.Chatted:Connect(function(Message)
+    if string.gsub(string.lower(Message),"%s","") ~= OPEN_PREFIX..OPEN_COMMAND then return end
+    Tests.Visible = not Tests.Visible
+end)
+
+--Set up Cmdr.
+ReplicatedStorage.ChildAdded:Connect(SetUpCmdr)
+SetUpCmdr()
